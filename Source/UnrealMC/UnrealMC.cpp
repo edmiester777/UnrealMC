d817c42e897c21f1c0f58107df80edab6cddce2a
edmiester777 <drtbkrdude472@hotmail.com>
2022-03-28 16:43:10 -0700

    Initial commit

---
+++ Source/UnrealMC/UnrealMC.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealMC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealMC, "UnrealMC" );
